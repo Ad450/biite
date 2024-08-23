80500a0bedb7e6bcffe21679e3bcf14daf26dc48
Ad450 <adjeimanue@gmail.com>
2024-08-23 10:31:32 +0000

    animate bar widgets of gains

--- lib/features/feed/widgets/bar.widget.dart
+++ lib/features/feed/widgets/bar.widget.dart
@@ -1,19 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class BarWidget extends StatelessWidget {
+class BarWidget extends StatefulWidget {
   const BarWidget({required this.gain, required this.color, super.key});
   final double gain;
   final Color color;
 
   @override
+  State<BarWidget> createState() => _BarWidgetState();
+}
+
+class _BarWidgetState extends State<BarWidget> with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _heightAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 500),
+    );
+
+    _heightAnimation = Tween<double>(begin: 0, end: (widget.gain / 100) * 215).animate(
+      CurvedAnimation(
+        parent: _animationController,
+        curve: Curves.linear,
+      ),
+    );
+
+    _animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 32.w,
-      height: (gain / 100) * 215,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(6),
-        color: color,
+    return AnimatedBuilder(
+      animation: _heightAnimation,
+      builder: (context, child) => Container(
+        width: 32,
+        height: _heightAnimation.value,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(6),
+          color: widget.color,
+        ),
       ),
     );
   }

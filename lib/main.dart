d36a333e21d5d39dc111b497bbdae2871d00aa33
Ad450 <adjeimanue@gmail.com>
2024-09-05 16:05:04 +0000

    configure di

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:biite/biite.dart';
+import 'package:biite/core/di/biite.di.dart';
 import 'package:biite/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -12,6 +13,7 @@ void main() async {
     await Firebase.initializeApp(
       options: DefaultFirebaseOptions.currentPlatform,
     );
+    configureDependencies();
   } catch (e) {
     debugPrint(e.toString());
   }
